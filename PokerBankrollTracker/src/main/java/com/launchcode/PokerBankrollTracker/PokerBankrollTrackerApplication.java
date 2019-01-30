ff31cc6dd0729f8e48c35bd19efec61c7fd3599a
Rob Green <rgreen0502@gmail.com>
2019-01-29 19:18:04 -0600

    Starting

---
+++ PokerBankrollTracker/src/main/java/com/launchcode/PokerBankrollTracker/PokerBankrollTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.launchcode.PokerBankrollTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerBankrollTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerBankrollTrackerApplication.class, args);
+	}
+
+}
+
---
+++ PokerBankrollTracker/src/test/java/com/launchcode/PokerBankrollTracker/PokerBankrollTrackerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.launchcode.PokerBankrollTracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PokerBankrollTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
